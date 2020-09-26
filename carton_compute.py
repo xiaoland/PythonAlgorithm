7ec3beb3a6e2ddce84d02e6ae718dd064855d250
Lanzhijiang <lanzhijiang@foxmail.com>
2020-09-26 14:43:50 +0800

    「更新」添加康拓逆运算

--- carton_compute.py
+++ carton_compute.py
@@ -49,6 +49,30 @@ class CartonCompute():
         :param length: 序列长度
         :return list
         """
+        index -= 1
+        appear = []
+        ans = []
+        for i in range(0, length):
+            now_factorial = self.get_factorial(length-i)
+            nac = index / now_factorial
+            index -= nac*now_factorial
+            
+            for j in range(1, length+1):
+                if j not in appear:
+                    write_in = j
+                    if nac == 0:
+                        break
+                    nac-=1
+            appear.append(write_in)
+            ans.append(write_in)
+        
+        a = ans[0]
+        ans.remove(ans[0])
+        ans.reverse()
+        ans.insert(0, a)
+        return ans
+            
 
 
-# print(CartonCompute().carton_opreation([4, 3, 2, 1], 4)) # -RESULT: Successful
+print(CartonCompute().carton_opreation([4, 3, 2, 1], 4)) # RESULT: Successful
+print(CartonCompute().inverse_opreation_of_carton(24, 5)) # RESULT: 
