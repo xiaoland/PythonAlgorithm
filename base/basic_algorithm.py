160e6e61bcaf74da164adee48d683153818e0eaa
Lanzhijiang <lanzhijiang@foxmail.com>
2020-09-28 21:38:42 +0800

    「添加」阶乘数据不符报错机制

--- base/basic_algorithm.py
+++ base/basic_algorithm.py
@@ -8,7 +8,18 @@ class BasicAlgorithm():
         pass
 
     def get_factorial(self, x):
+
+        """
+        计算阶乘
+        :param x: x!的x (x>0)
+        :return int
+        """
+        if x < 0:
+            raise Exception("Input can't small than 0!")
+        elif x == 0:
+            return 1
+        
         result = 1
-        for i in range(0, x):
-            result = result*i
+        for i in range(1, x):
+            result*=i
         return result
